e8da467cb7853e592c6ea446dd24248ec42370b9
KinectTheUnknown <34723558+KinectTheUnknown@users.noreply.github.com>
2021-03-10 22:47:08 -1000

    Updated deps and contract

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -1,18 +1,25 @@
 pragma solidity ^0.5.0;
 
-import "./ERC721Full.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Color is ERC721Full {
-  string[] public colors;
-  mapping(string => bool) _colorExists;
+contract Color is ERC721 {
+  struct Set {
+    uint size;
+    mapping (string => uint) _items;
+  }
+  Set ids = Set(0);
+  function addItem(string memory _item) private returns(uint) {
+    require(!hasItem(_item));
 
-  constructor() ERC721Full("Color", "COLOR") public {}
-
-  // e.g. color = "#FFFFFF"
+    return ids._items[_item] = ++ids.size;
+  }
+  function hasItem(string memory _item) private returns(bool) {
+    return ids._items[_item] > 0;
+  }
+  constructor() ERC721() public {}
+  // E.G. color = "#FFFFFF"
   function mint(string memory _color) public {
-    require(!_colorExists[_color]);
-    uint _id = colors.push(_color);
+    uint _id = addItem(_color);
     _mint(msg.sender, _id);
-    _colorExists[_color] = true;
   }
 }
